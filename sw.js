3222711bc7509b32f225c85596fbc8fcb87c9105
Mateusz <mknieszner@gmail.com>
2019-09-14 21:58:41 +0200

    svg update

--- sw.js
+++ sw.js
@@ -1,23 +1,5 @@
-var CACHE_STATIC_NAME = 'static-v08';
-var STATIC_FILES = [
-    '/',
-    '/index.html',
-    '/manifest.json',
-    '/js/utilities.js',
-    '/img/favicon/favicon.ico',
-    '/css/style.css',
-    '/img/icons/Icon-144.png',
-    '/img/icons/Icon-512.png',
-    '/img/svg/me.svg',
-    '/img/svg/education.svg',
-    '/img/svg/personal-projects.svg',
-    '/img/svg/work.svg',
-    '/img/svg/free-time.svg',
-    '/img/card-back.png',
-    '/img/main-back.png',
-    '/img/no-javascript.svg',
-    'https://fonts.googleapis.com/css?family=Raleway:100,300,400,700,900&subset=latin-ext'
-];
+var CACHE_STATIC_NAME = 'static-v09';
+var STATIC_FILES = []; // todo: update
 
 self.addEventListener('install', function (event) {
     console.log('[Service Worker] Installing Service Worker ...', event);
